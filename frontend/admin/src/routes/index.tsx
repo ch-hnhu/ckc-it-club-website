c678c7d2413fdb461a8d7c16b9fa045441392e53
Chung Hue Nhu <0306231416@caothang.edu.vn>
2026-04-20 09:00:07 -0700

    ui: add CreateUser form and reusable Combobox component with search and multi-select support

--- frontend/admin/src/routes/index.tsx
+++ frontend/admin/src/routes/index.tsx
@@ -5,6 +5,7 @@ import FacultyList from "@/pages/faculty/FacultyList";
 import MajorList from "@/pages/major/MajorList";
 import SchoolClassList from "@/pages/school-class/SchoolClassList";
 import UserList from "@/pages/user/UserList";
+import CreateUser from "@/pages/user/CreateUser";
 import ApplicationRequestsPage from "@/pages/recruitment/ApplicationRequestsPage";
 import ApplicationDetailPage from "@/pages/recruitment/ApplicationDetailPage";
 import ApplicationQuestionsPage from "@/pages/recruitment/ApplicationQuestionsPage";
@@ -35,6 +36,10 @@ const router = createBrowserRouter([
 			{
 				path: "users",
 				element: <UserList />,
+			},
+			{
+				path: "users/create",
+				element: <CreateUser />,
 			},
 			{
 				path: "departments",
