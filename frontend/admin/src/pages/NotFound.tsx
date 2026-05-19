29b51e93f9489576cf391882f83304c2e3cf21de
Chung Hue Nhu <0306231416@caothang.edu.vn>
2026-05-18 21:26:59 -0700

    fix: 404 - nút back trả về trang trc đó thay vì về trang chủ

--- frontend/admin/src/pages/NotFound.tsx
+++ frontend/admin/src/pages/NotFound.tsx
@@ -1,16 +1,16 @@
 import { Button } from "@/components/ui/button";
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 export default function NotFound() {
+	const navigate = useNavigate();
+
 	return (
 		<div className='h-screen flex flex-col items-center justify-center'>
 			<h1 className='text-6xl font-bold'>404</h1>
 			<p className='text-lg mt-2'>Trang không tồn tại!</p>
-			<Link to='/' className='mt-4 inline-block' style={{ textDecoration: "none" }}>
-				<Button variant='outline' size='lg' className='h-10 lg:flex'>
-					Về trang chủ
-				</Button>
-			</Link>
+			<Button variant='outline' size='lg' className='mt-4 h-10 lg:flex' onClick={() => navigate(-1)}>
+				Quay lại
+			</Button>
 		</div>
 	);
 }
